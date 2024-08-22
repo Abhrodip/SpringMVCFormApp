b03bf7a31fe731b0e4d576503af3560173ffe83d
Abhrodip <98941200+Abhrodip@users.noreply.github.com>
2024-08-22 11:54:01 +0530

    Update SOP UserController.java

--- src/main/java/com/ashok/controller/UserController.java
+++ src/main/java/com/ashok/controller/UserController.java
@@ -13,7 +13,7 @@ public class UserController {
 
 	@RequestMapping(value = "/displayForm.htm", method = RequestMethod.GET)
 	public String displayForm(Model model) {
-		System.out.println("Checking SOP for JEnkins build");
+		System.out.println("Checking SOP for JEnkins build.....");
 		System.out.println("hi");
 		int i = 10;
 		model.addAttribute("user", new User());
